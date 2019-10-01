b655523b551aa5f4db99b912981556372e52fa5b
Brian Bansenauer <brianb@cascadia.edu>
2019-09-30 22:18:43 -0700

    update TODOs

--- app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
+++ app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
@@ -4,34 +4,38 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class CreateMessageActivity extends Activity {
 
-    //TODO: Declare instance variables for EditText and two Button Views
+    //Declare instance variables for EditText and two Button Views
     EditText messageView;
+    Button buttonToApp;
+    Button buttonToActivity;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_message);
-        //TODO: assign findViewById values to all View variables
+        //TODO: assign findViewById values to all instance variables
         messageView = (EditText)findViewById(R.id.message);
 
+        
+        //TODO: Add a setOnClickListener to each Button
 
-        //TODO: Add an Event Listener to each Button
-
-    }
-
-    //TODO: Create a "to Activity" method that uses an explicit Intent
-    //      to call the ReceiveMessageActivity
-    public void onSendMessageToActivity(View view){
 
     }
 
-    //Call onSendMessage() when the "to App" button is clicked
-    public void onSendMessageToApp(View view) {
+    //Call sendMessageToActivity when the "to Activity" button is clicked
+    public void sendMessageToActivity(){
+        //TODO: Use an explicit Intent to invoke the ReceiveMessageActivity with the message
+
+    }
+
+    //Call sendMessageToApp() when the "to App" button is clicked
+    public void sendMessageToApp() {
         String messageText = messageView.getText().toString();
         Intent intent = new Intent(Intent.ACTION_SEND); intent.setType("text/plain");
         intent.putExtra(Intent.EXTRA_TEXT, messageText);
