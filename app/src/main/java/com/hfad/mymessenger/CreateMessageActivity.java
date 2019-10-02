517d73e70910b5c86815a98ec810e684e59a4a8e
gomezfox <stephen@gomezfox.net>
2019-10-01 20:12:07 -0700

    Connected activities with intent

--- app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
+++ app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
@@ -21,17 +21,33 @@ public class CreateMessageActivity extends Activity {
         setContentView(R.layout.activity_create_message);
         //TODO: assign findViewById values to all instance variables
         messageView = (EditText)findViewById(R.id.message);
+        buttonToApp = (Button)findViewById(R.id.sendImplicit);
+        buttonToActivity = (Button)findViewById(R.id.sendExplicit);
 
         
         //TODO: Add a setOnClickListener to each Button
-
+        buttonToActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendMessageToActivity();
+            }
+        });
+        buttonToApp.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                sendMessageToApp();
+            }
+        });
 
     }
 
     //Call sendMessageToActivity when the "to Activity" button is clicked
     public void sendMessageToActivity(){
         //TODO: Use an explicit Intent to invoke the ReceiveMessageActivity with the message
-
+        String messageText = messageView.getText().toString();
+        Intent explicitIntent = new Intent(getApplicationContext(),ReceiveMessageActivity.class);
+        explicitIntent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
+        startActivity(explicitIntent);
     }
 
     //Call sendMessageToApp() when the "to App" button is clicked
