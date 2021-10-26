f15aba3e4efa899db483840c8a7357e3b19412b9
FABRICE KALVANDA <fabricekalvanda@gmail.com>
2021-10-25 21:16:50 -0700

    First Commit

--- app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
+++ app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class CreateMessageActivity extends Activity {
     public static final String MESSAGE = "MESSAGE";
@@ -22,28 +23,34 @@ public class CreateMessageActivity extends Activity {
         setContentView(R.layout.activity_create_message);
         //TODO: assign findViewById values to all instance variables for all widgets
         messageView = findViewById(R.id.message);
-        //buttonSendToApp = ...
-        //buttonSendToActivity = ...
+        buttonSendToOtherApp = findViewById(R.id.sendImplicit);
+        buttonSendToActivity = findViewById(R.id.sendExplicit);
 
-        
+
         //TODO: Add a setOnClickListener to each Button
-
-
+        buttonSendToOtherApp.setOnClickListener(v -> sendMessageToOtherApp());
+        buttonSendToActivity.setOnClickListener(v -> sendMessageToActivity());
     }
 
     //Call sendMessageToActivity when the "to Activity" button is clicked
     public void sendMessageToActivity(){
         //TODO: Create an explicit Intent for ReceiveMessageActivity; add the TextView message and start the new Activity
-
+       String messageText = messageView.getText().toString();
+       Intent ReceiveMessageActivity = new Intent(this, com.hfad.mymessenger.ReceiveMessageActivity.class);
+       ReceiveMessageActivity.putExtra(MESSAGE, messageText);
+       ReceiveMessageActivity.putExtra("urgent", true);
+       startActivity(ReceiveMessageActivity);
     }
 
     //Creates an IMPLICIT intent, adds the textView's message as a String, and sends it to a "Chooser" window for the user to pick
     public void sendMessageToOtherApp() {
         String messageText = messageView.getText().toString();
-        Intent intent = new Intent(Intent.ACTION_SEND); intent.setType("text/plain");
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
         intent.putExtra(Intent.EXTRA_TEXT, messageText);
         String chooserTitle = getString(R.string.chooser);
         Intent chosenIntent = Intent.createChooser(intent, chooserTitle);
         startActivity(chosenIntent);
     }
+
 }
